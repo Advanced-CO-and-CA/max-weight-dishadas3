ee84bab7749f15f688375bd89aaf7b8750b23fa5
dishadas3 <54740307+dishadas3@users.noreply.github.com>
2019-10-12 13:07:57 +0530

    Add comments and STR in =NUM and =WEIGHT
    Comments added and STR into required addresses in memory done

--- arm_lab_cs18m509_assignment_3.s
+++ arm_lab_cs18m509_assignment_3.s
@@ -4,17 +4,21 @@
 * Guide: Prof. Madhumutyam IITM, PACE
 ******************************************************************************/
 
+@NUM loaded into R5 as OUTPUT
+@WEIGHT loaded into R6 as OUTPUT 
+
+
   @ BSS section
       .bss
 
   @ DATA SECTION
     .data
 data_start: .word 0x205A15E3 ;  #(0010 0000 0101 1010 0001 0101 1101 0011 – 13)
-            .word 0x256C8700 ;  #(0010 0101 0110 1100 1000 0111 0000 0000 – 11)
-data_end:   .word 0x295468F2 ;  #(0010 1001 0101 0100 0110 1000 1111 0010 – 14)
+            .word 0x295468F2 ;  #(0010 1001 0101 0100 0110 1000 1111 0010 – 14)  
+data_end:   .word 0x256C8700 ;  #(0010 0101 0110 1100 1000 0111 0000 0000 – 11)     more numbers can be added and swapped in order  
 
-Output: NUM:
-        WEIGHT: 
+Output: NUM: .word 0x0;
+        WEIGHT: .word 0x0;
 		
   @ TEXT section
       .text
@@ -23,10 +27,9 @@ Output: NUM:
 
 
 _main:
-    LDR R0, = data_start    @R0 holds start load address
-	LDR R5, = NUM           @R5 holds num with max weight in NUM addr   (OUTPUT NUM)
-    LDR R9, = WEIGHT        @R9 holds max weight in WEIGHT addr         (OUTPUT MAX WEIGHT)
-	MOV R6, #0              @R6 holds max weight
+    LDR R0, = data_start        @R0 holds start load address
+	MOV R5, #0              @R5 holds num with max weight     (FINALLY will LOAD output from NUM)
+	MOV R6, #0              @R6 holds max weight              (FINALLY will LOAD output from WEIGHT)
 	LDR R7, = data_end      @R7 holds end load address
 	LDR R8, [R7]            @R8 holds last input value
 	
@@ -51,11 +54,16 @@ _main:
 	
 	update_max_weight:      @update max weight
 	MOV R6, R2              @new max weight in R2 copied into max weight register R6
+	MOV R5, R4              @new number with max weight updated in R5
 	CMP R4, R8              @check terminate
 	BEQ complete
 	B load                  @load next number
 	
 	complete:
-	STR R4, [R5]            @copy at NUM from R4    (number with max weight)
-	STR R6, [R9]            @copy at WEIGHT from R6 (maximum weight)
+	LDR R9, = WEIGHT        @R9 holds WEIGHT addr  (OUTPUT MAX WEIGHT)
+	STR R6, [R9]            @copy at WEIGHT from R6 which has max weight
+        LDR R10, = NUM          @R10 holds NUM addr   (OUTPUT NUM)
+	STR R5, [R10]           @copy at WEIGHT from R6 (maximum weight)
+	LDR R6, [R9]            @LOAD R6 AND CHECK FINAL VALUE AT WEIGHT
+	LDR R5, [R10]           @LOAD R5 AND CHECK FINAL VALUE AT NUM
 	swi 0x11
